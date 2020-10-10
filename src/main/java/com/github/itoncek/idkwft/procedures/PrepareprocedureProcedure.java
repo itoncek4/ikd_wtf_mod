25b63a30fe8d82353c529e312057a152e7fc57e7
itoncek4 <51187692+itoncek4@users.noreply.github.com>
2020-10-10 15:45:11 +0200

    update

---
+++ src/main/java/com/github/itoncek/idkwft/procedures/PrepareprocedureProcedure.java
@@ -0,0 +1,15 @@
+package com.github.itoncek.idkwft.procedures;
+
+import java.util.Map;
+
+import com.github.itoncek.idkwft.IdkwftModElements;
+
+@IdkwftModElements.ModElement.Tag
+public class PrepareprocedureProcedure extends IdkwftModElements.ModElement {
+	public PrepareprocedureProcedure(IdkwftModElements instance) {
+		super(instance, 2);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+	}
+}
